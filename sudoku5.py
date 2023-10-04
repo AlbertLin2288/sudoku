6015dd847c16742d345aeba400de99789ec6b66f
AlbertLin2288 <lingchenyu2288@gmail.com>
2023-10-04 20:21:00 +0800

    modified:   sudoku5.py add basic structure

--- sudoku5.py
+++ sudoku5.py
@@ -3,3 +3,11 @@
 # but two types of rule
 # in a row/column/box: 1-9
 # for each number: avilable slot
+
+class Sudoku:
+    def __init__(self) -> None:
+        pass
+
+class number:
+    def __init__(self) -> None:
+        pass
