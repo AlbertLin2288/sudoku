eeb5ee40b697b4677038d93de0c6d5c7250df8bb
AlbertLin2288 <lingchenyu2288@gmail.com>
2023-10-05 21:34:41 +0800

    modified:   sudoku5.py

--- sudoku5.py
+++ sudoku5.py
@@ -16,6 +16,8 @@ class Sudoku:
             self.no_dupe_rules = {} # {id:[Number, Number..., int, int...]}
                                     # there can't be dupelicate in list
             self.id = 0 # largest unused id
+            self.has_ids = []
+            self.no_dupe_ids = []
             for r in range(9): # add rules to rows
                 for i in range(9):
                     self.add_has_rule(i, zip([r]*9,range(9)))
@@ -28,6 +30,7 @@ class Sudoku:
                 for i in range(9):
                     has_rule = [i]
                     self.has_rules[self.id] = has_rule
+                    self.has_ids.append(self.id)
                     for p in range(9): # the cord in the flattened block
                         number = self.board[block//3*3+p//3][block%3*3+p%3]
                         has_rule.append(number)
@@ -35,6 +38,7 @@ class Sudoku:
                     self.id += 1
                 no_dupe_rule = []
                 self.no_dupe_rules[self.id] = no_dupe_rule
+                self.no_dupe_ids.append(self.id)
                 for p in range(9):
                     number = self.board[block//3*3+p//3][block%3*3+p%3]
                     no_dupe_rule.append(number)
@@ -50,6 +54,8 @@ class Sudoku:
         self.has_rules = {}
         self.no_dupe_rules = {}
         self.id = copy.id
+        self.has_ids = copy.has_ids
+        self.no_dupe_ids = copy.no_dupe_ids
         for r in range(9):
             row = []
             for c,i in enumerate(copy.board[r]):
@@ -84,6 +90,7 @@ class Sudoku:
     def add_has_rule(self, value, cords):
         has_rule = [value]
         self.has_rules[self.id] = has_rule
+        self.has_ids.append(self.id)
         for r,c in cords:
             has_rule.append(self.board[r][c])
             self.board[r][c].has_rules[self.id] = has_rule
@@ -92,24 +99,38 @@ class Sudoku:
     def add_no_dupe_rule(self, values, cords):
         no_dupe_rule = list(values)
         self.no_dupe_rules[self.id] = no_dupe_rule
+        self.no_dupe_ids.append(self.id)
         for r,c in cords:
             no_dupe_rule.append(self.board[r][c])
             self.board[r][c].no_dupe_rules[self.id] = no_dupe_rule
         self.id += 1
 
-    def update_has_rules(self, ids):
-        """update the has_rules with id in ids
+    def update_has_rules(self):
+        """update the has_rules
         check if there is only one element in rule
+            update: check this when removing element in rule
         check if there is child of rule"""
+        for i in self.has_ids:
+            has_rule = self.has_rules[i]
 
     def update_do_dupe_rules(self, ids):
-        """update the no_dupe_rules with id in ids
+        """update the no_dupe_rules
         check if there is child has_rule"""
+
+    def has_rule_true(self, has_id):
+        """has_rule is satisifled, so delete it"""
+        self.has_ids.remove(has_id)
+        has_rules = self.has_rules.pop(has_id)
+        for i in has_rules[1:]:
+            i.has_rules.pop(has_id)
+
+    def error(self):
+        """an error has occured"""
 
 
 class Number:
     """The number"""
-    def __init__(self, board, r, c) -> None:
+    def __init__(self, board:Sudoku, r, c) -> None:
         self.board = board
         self.pos = (r,c)
         self.r, self.c = self.pos
@@ -117,3 +138,21 @@ class Number:
         self.no_dupe_rules = {}
         self.possible = [1,2,3,4,5,6,7,8,9]
 
+    def set(self):
+        """replace self with int"""
+
+    def set2(self, value):
+        """set self to value"""
+        self.possible = value
+        update_list = set() # don't ask me why this is a set
+        for i in self.has_rules:
+            if value == self.has_rules[i][0]: # has_rule satisified
+                self.board.has_rule_true(i)
+            else:
+                has_rule = self.has_rules[i]
+                has_rule.remove(self)
+                if len(has_rule) == 1:
+                    self.board.error()
+                    return None
+                elif len(has_rule) == 2:
+                    update_list.add(has_rule[1])
